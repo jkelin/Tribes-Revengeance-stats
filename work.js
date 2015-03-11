34f8e58b43975853241d4cc40262bc2efe2b690a
fireantik <windowmaker001@gmail.com>
2015-03-11 18:29:14 +0100

    a

--- work.js
+++ work.js
@@ -8,6 +8,7 @@ var atob = require('atob')
 var exphbs  = require('express-handlebars');
 var url = require('url');
 var moment = require('moment');
+var q = require('q');
 
 var timeoutMs = 1000;
 
@@ -280,10 +281,20 @@ app.use (function(req, res, next) {
 });
 
 app.get('/', function (req, res) {
-	Player.find().sort({kills:-1}).limit(20).exec(function(err,data){
-		if(err) throw err;
+	var compDate = new Date();
+	compDate.setMinutes(compDate.getMinutes() - 2);
+
+	var promises = [
+		Player.find().sort({kills:-1}).limit(20).exec(),
+		Player.find().sort({minutesonline:-1}).limit(20).exec(),
+		Server.find().where({lastseen:{"$gte": compDate}}).limit(20).exec()
+	];
+
+	q.all(promises).then(function(data) {
 		res.render('home', {
-			data:data,
+			playersKills:data[0],
+			playersTime:data[1],
+			servers:data[2],
 			helpers:helpers
 		});
 	});
