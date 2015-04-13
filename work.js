0ec035bdde6f1ac985d3a19962f5e949dbea3b55
fireantik <windowmaker001@gmail.com>
2015-04-13 18:46:51 +0200

    index caching and old meter

--- work.js
+++ work.js
@@ -418,29 +418,38 @@ app.use (function(req, res, next) {
 });
 
 app.get('/', function (req, res) {
-	var compDate = new Date();
-	compDate.setMinutes(compDate.getMinutes() - 2);
+	var r = myCache.get("index");
+	if(r.index !== undefined){
+		res.render('home', r.index);
+	}
+	else{
+		var compDate = new Date();
+		compDate.setMinutes(compDate.getMinutes() - 2);
 
-	var promises = [
-		Player.find().sort({kills:-1}).limit(20).exec(),
-		Player.find().sort({minutesonline:-1}).limit(20).exec(),
-		Server.find().where({lastseen:{"$gte": compDate}}).limit(20).exec()
-	];
+		var promises = [
+			Player.find().sort({kills:-1}).limit(20).exec(),
+			Player.find().sort({minutesonline:-1}).limit(20).exec(),
+			Server.find().where({lastseen:{"$gte": compDate}}).limit(20).exec()
+		];
 
-	q.all(promises).then(function(data) {
-		//console.log("then");
-		res.render('home', {
-			playersKills:data[0],
-			playersTime:data[1],
-			servers:data[2],
-			news:tribes_news.slice(0, 5),
-			helpers:helpers
-		});
-	}).catch(function (error) {
-		console.log("catch");
-		next(error);
-	})
-	.done();
+		q.all(promises).then(function(data) {
+			//console.log("then");
+			var obj = {
+				playersKills:data[0],
+				playersTime:data[1],
+				servers:data[2],
+				news:tribes_news.slice(0, 5),
+				helpers:helpers,
+				now: new Date(Date.now())
+			};
+			res.render('home', obj);
+			myCache.set("index",obj,180);
+		}).catch(function (error) {
+			console.log("catch");
+			next(error);
+		})
+		.done();
+	}
 })
 
 app.get('/player/:name', cacher.cache(false), function (req, res) {
