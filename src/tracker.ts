b69fa8f608246f15717ac88b267f2e3861e7d7f9
Jan Kelin <jan@jkelin.com>
2020-04-05 17:14:15 +0200

    Refactor tracker

--- src/tracker.ts
+++ src/tracker.ts
@@ -23,10 +23,58 @@ export async function handleTribesServerData(data: ITribesServerQueryResponse) {
     return console.error('[handleTribesServerData] Server does not have an id', data);
   }
 
-  const id = data.ip + ':' + data.hostport;
+  const serverAddress = data.ip + ':' + data.hostport;
 
-  console.debug('Handling data from', id);
+  console.debug('Handling server data from', serverAddress);
 
+  const server = await findOrCreateServer(serverAddress);
+
+  const minutesSinceLastTiming = (Date.now() - server.lastTiming.getTime()) / (60 * 1000);
+
+  server.name = data.hostname;
+  server.adminname = data.adminname;
+  server.adminemail = data.adminemail;
+  server.ip = data.ip;
+  server.port = data.hostport;
+  server.maxplayers = data.maxplayers;
+  server.lastseen = new Date();
+  server.minutesonline = (server.minutesonline || 0) + minutesSinceLastTiming;
+
+  if (!server.country) {
+    const location = geoip.lookup(server.ip);
+    server.country = location.country.toLowerCase();
+  }
+
+  server.lastdata = data;
+
+  if (server.lastdata.mapname) {
+    server.lastdata.mapnamefull = getFullMapName(server.lastdata.mapname);
+  }
+
+  if (data.players.length !== persistentPlayerCounts[`${server.ip}:${server.port}`]) {
+    Events.next({
+      type: 'player-count-change',
+      data: {
+        server: `${server.ip}:${server.port}`,
+        players: data.players.length,
+        origin: selfEventId,
+      },
+    });
+
+    persistentPlayerCounts[`${server.ip}:${server.port}`] = data.players.length;
+  }
+
+  const promises = [pushPlayersTrackings(serverAddress, data), ...data.players.map(timePlayer)];
+
+  await Promise.all(promises);
+  await server.save();
+
+  console.debug('Saved server', serverAddress);
+}
+
+const lastTrackings: Record<string, number> = {};
+
+async function findOrCreateServer(id: string): Promise<IServerModel> {
   let server: IServerModel = await Server.where('_id')
     .equals(id)
     .findOne()
@@ -40,65 +88,9 @@ export async function handleTribesServerData(data: ITribesServerQueryResponse) {
     } as IServer);
   }
 
-  server.name = data.hostname;
-  server.adminname = data.adminname;
-  server.adminemail = data.adminemail;
-  server.ip = data.ip;
-  server.port = data.hostport;
-  server.maxplayers = data.maxplayers;
-  server.lastseen = new Date();
-
-  if (Date.now() >= server.lastTiming.getTime() + 60 * 1000) {
-    server.minutesonline++;
-    server.lastTiming = new Date();
-    console.debug('Timing server', id);
-  } else {
-    console.debug(
-      'Could not time server because lastTiming is',
-      server.lastTiming,
-      'and now is',
-      new Date(),
-      'while needed is',
-      new Date(Date.now() - 60 * 1000),
-      'diff:',
-      server.lastTiming.getTime() - new Date(Date.now() - 60 * 1000).getTime()
-    );
-  }
-
-  await server.save();
-
-  if (data.players.length !== persistentPlayerCounts[`${server.ip}:${server.port}`]) {
-    Events.next({
-      type: 'player-count-change',
-      data: {
-        server: `${server.ip}:${server.port}`,
-        players: data.players.length,
-        origin: selfEventId,
-      },
-    });
-  }
-
-  persistentPlayerCounts[`${server.ip}:${server.port}`] = data.players.length;
-  server.lastdata = data;
-
-  if (server.lastdata.mapname) {
-    server.lastdata.mapnamefull = getFullMapName(server.lastdata.mapname);
-  }
-
-  const promises = [pushPlayersTrackings(id, data), ...data.players.map(timePlayer)];
-
-  await Promise.all(promises);
-
-  if (!server.country) {
-    const location = geoip.lookup(server.ip);
-    server.country = location.country.toLowerCase();
-  }
-
-  await server.save();
-  console.debug('Saved server', id);
+  return server;
 }
 
-const lastTrackings: Record<string, number> = {};
 export async function pushPlayersTrackings(serverIdIn: string, data: ITribesServerQueryResponse) {
   if (!lastTrackings[serverIdIn]) {
     lastTrackings[serverIdIn] = 0;
@@ -131,128 +123,151 @@ export async function timePlayer(player: IUploadedPlayer) {
   if (!player.player) {
     return console.error('[timePlayer] Player does not have a name', player);
   }
-  let pl = await Player.where('_id')
-    .equals(player.player)
-    .findOne()
-    .exec();
 
-  if (pl === null) {
-    pl = new Player({
-      _id: player.player,
-      normalizedName: cleanPlayerName(player.player + ''),
-      stats: {},
-      score: 0,
-      kills: 0,
-      deaths: 0,
-      offense: 0,
-      defense: 0,
-      style: 0,
-      minutesonline: 0,
-      lastTiming: new Date(),
-    } as IPlayer);
-  }
+  const pl = await findOrCreatePlayer(player.name);
 
-  if (Date.now() >= pl.lastTiming.getTime() + 60 * 1000) {
-    pl.minutesonline++;
-    pl.lastTiming = new Date();
-    console.debug('Timing player', player.player);
-  }
-
+  const minutesSinceLastTiming = (Date.now() - pl.lastTiming.getTime()) / (60 * 1000);
   pl.normalizedName = cleanPlayerName(player.player + '');
+  pl.minutesonline = (pl.minutesonline || 0) + minutesSinceLastTiming;
+  pl.lastTiming = new Date();
   pl.lastseen = new Date();
 
   await pl.save();
+
+  console.debug('Timed player', player.player);
 }
 
-export async function handlePlayer(input: IUploadedPlayer, ip: string, port: number) {
-  console.debug('handling player', input);
-  if (!input.name) {
-    return console.error('Player does not have a name');
+async function findOrCreatePlayer(player: string): Promise<IPlayerModel> {
+  let pl = await findPlayerById(player);
+
+  if (!pl) {
+    pl = createPlayer(player);
   }
 
-  let player = await Player.where('_id')
-    .equals(input.name)
+  return pl;
+}
+
+function createPlayer(player: string): IPlayerModel {
+  return new Player({
+    _id: player,
+    normalizedName: cleanPlayerName(player + ''),
+    stats: {},
+    score: 0,
+    kills: 0,
+    deaths: 0,
+    offense: 0,
+    defense: 0,
+    style: 0,
+    minutesonline: 0,
+    lastTiming: new Date(),
+  } as IPlayer);
+}
+
+async function findPlayerById(player: string): Promise<IPlayerModel> {
+  return await Player.where('_id')
+    .equals(player)
     .findOne()
     .exec();
+}
 
-  const changeCountry = false;
-  if (player === null) {
-    player = new Player({
-      _id: input.name,
-      normalizedName: cleanPlayerName(input.name),
-      stats: {},
-      score: 0,
-      kills: 0,
-      deaths: 0,
-      offense: 0,
-      defense: 0,
-      style: 0,
-      minutesonline: 20,
-      lastTiming: new Date(),
-    } as IPlayer);
-  }
-
+function updatePlayerStatsFromInput(player: IPlayerModel, input: IUploadedPlayer) {
   if (player.offense === undefined) {
     player.offense = 0;
   }
 
   player.normalizedName = cleanPlayerName(input.name);
   player.ip = input.ip;
-  player.lastserver = ip + ':' + port;
+
+  if (!player.score) {
+    player.score = 0;
+  }
   player.score += input.score;
+
+  if (!player.kills) {
+    player.kills = 0;
+  }
   player.kills += input.kills;
+
+  if (!player.deaths) {
+    player.deaths = 0;
+  }
   player.deaths += input.deaths;
+
+  if (!player.offense) {
+    player.offense = 0;
+  }
   player.offense += input.offense;
+
+  if (!player.defense) {
+    player.defense = 0;
+  }
   player.defense += input.defense;
+
+  if (!player.style) {
+    player.style = 0;
+  }
   player.style += input.style;
+
   player.lastseen = new Date();
 
   if (!player.stats) {
     player.stats = {};
   }
 
-  if (player.stats.StatHighestSpeed === undefined) {
+  if (!player.stats.StatHighestSpeed) {
     player.stats.StatHighestSpeed = 0;
   }
 
-  const highestSpeed =
-    input['StatClasses.StatHighestSpeed'] === undefined ? 0 : parseInt(input['StatClasses.StatHighestSpeed'] + '', 10);
+  const highestSpeed = !input['StatClasses.StatHighestSpeed']
+    ? 0
+    : parseInt(input['StatClasses.StatHighestSpeed'] + '', 10);
+
   if (highestSpeed > player.stats.StatHighestSpeed) {
     player.stats.StatHighestSpeed = highestSpeed;
     player.markModified('stats');
   }
 
   // tslint:disable-next-line:forin
-  for (const i in input) {
-    const value = input[i];
-    if (typeof value !== 'number') {
+  for (const statFullName in input) {
+    const statValue = input[statFullName];
+    if (typeof statValue !== 'number') {
       continue;
     }
 
-    console.debug('handle player stat', { name: i, value });
-    if (i === 'StatClasses.StatHighestSpeed') {
+    console.debug('Handle player stat', { statName: statFullName, statValue });
+    if (statFullName === 'StatClasses.StatHighestSpeed') {
       continue;
     }
 
-    if (i.indexOf('.') !== -1) {
-      const name = i.split('.')[1];
+    if (statFullName.includes('.')) {
+      const statName = statFullName.split('.')[1];
 
-      if (!player.stats) {
-        player.stats = {};
+      if (!player.stats[statName]) {
+        player.stats[statName] = 0;
       }
 
-      if (!player.stats[name]) {
-        player.stats[name] = 0;
-      }
-
-      player.stats[name] += value;
+      player.stats[statName] += statValue;
       // console.log("addded",name,value);
       player.markModified('stats');
     }
   }
-  console.debug('statted', input.name);
+}
+
+export async function handlePlayer(input: IUploadedPlayer, serverIp: string, serverPort: number) {
+  console.debug('Handling player input', input);
+
+  if (!input.name) {
+    return console.error('Player does not have a name');
+  }
+
+  const player = await findOrCreatePlayer(input.name);
+
+  player.lastserver = serverIp + ':' + serverPort;
+
+  updatePlayerStatsFromInput(player, input);
 
   await player.save();
+  console.debug('Updated stats for', input.name);
 }
 
 export async function addServerLastFullReport(ip: string, port: number) {
@@ -293,7 +308,7 @@ export async function saveMatchResult(ip: string, port: number, fullReport: IUpl
   const id = ip + ':' + port;
 
   // Server.where({ _id: '45.32.157.166:8777' })
-  const server = await Server.where('_id')
+  const server: IServerModel = await Server.where('_id')
     .equals(id)
     .findOne()
     .exec();
